f2cf7d166137ed398af973a71360ea9e46cce0af
jihannayy <140767336+jihannayy@users.noreply.github.com>
2024-02-16 09:31:15 +0700

    Menampilkan tulisan dan HTML

---
+++ lat01.php
@@ -0,0 +1,14 @@
+<?php
+
+echo 'Belajar PHP';
+
+echo '<br>';
+
+echo 'Saya siswa SMK';
+echo '<h1 style ="background-color:red">Belajar PHP itu Mudah</h1>
+    <p style= "background-color:blue">saya suka belajar progam</p>
+';
+echo 'saya belajar'.'PHP'.'<br>';
+echo 2019 + 2;
+
+?>
