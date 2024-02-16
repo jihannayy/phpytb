a6b3720bf8a242dab08a88d1ac00875b7343b3ba
jihannayy <140767336+jihannayy@users.noreply.github.com>
2024-02-16 11:10:05 +0700

    operator PHP

---
+++ lat04.php
@@ -0,0 +1,53 @@
+<?php
+
+// OPERATOR MATEMATIKA
+
+$a = 2;
+$b = 2;
+
+$c = $a + $b;
+
+echo $c. '<br>';
+
+$c = $a - $b;
+echo $c. '<br>';
+
+$c = $a * $b;
+echo $c. '<br>';
+
+$c = $a / $b;
+echo $c. '<br>';
+
+$c = $a % $b;
+echo $c. '<br>';
+
+// OPERATOR LOGIKA
+
+$c = $a < $b;
+echo $c. '<br>';
+
+$c = $a > $b;
+echo $c. '<br>';
+
+$c = $a == $b;
+echo $c. '<br>';
+
+$c = $a != $b;
+echo $c. '<br>';
+
+// INCREMENT
+
+$a++;
+echo $a. '<br>';
+
+// OPERATOR STRING
+
+$kata = 'kota';
+$kota = 'baya';
+
+$hasil = $kata.$kota;
+
+$hasil. ='KOTA PAHLAWAN';
+echo $hasil;
+
+?>
