e66477f01a53c5fb8c231342dd6f7a135db51dba
jihannayy <140767336+jihannayy@users.noreply.github.com>
2024-02-16 09:49:06 +0700

    Variabel tipe data dan Komentar

---
+++ lat02.php
@@ -0,0 +1,20 @@
+<?php
+
+$tulisan = 'Saya belajar';
+echo $tulisan. '<br>';
+
+$angka = '2019';
+echo 'tahun'.$angka.'<br>';
+
+// var_dump($tulisan);
+
+echo '<br>';
+
+// var_dump((int)$angka+2);
+
+
+
+
+
+
+?>
