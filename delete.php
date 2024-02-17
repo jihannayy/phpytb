0a25919151eaa02d736643cd9aef1ab8b4581fd4
jihannayy <140767336+jihannayy@users.noreply.github.com>
2024-02-17 19:18:14 +0700

    DELETE DATA

---
+++ delete.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once "../function.php";
+
+$sql = "DELETE FROM tblkategori WHERE idkategori = $id ";
+
+$result = mysqli_query($koneksi, $sql);
+
+header ("location:http:localhost/phpytb/restoran/kategori/select.php");
+
+
+
+
+
+?>
