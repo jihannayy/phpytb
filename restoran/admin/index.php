9688a85432dfe62770b187ede7c711eac5e6b9ed
jihannayy <140767336+jihannayy@users.noreply.github.com>
2024-02-23 02:27:03 +0700

    Admin Page Level Akses aplikasi Restoran

--- restoran/admin/index.php
+++ restoran/admin/index.php
@@ -21,17 +21,92 @@
 <body>
     <div class="container">
 
-        <div class="row">
+        <div class="row mt-4">
             <div class="col-md-3">
-                <h2>Restoran SMENDA</h2>
+                <a href="index.php"><h3>Admin Page</a></h3>
             </div>
 
-            <div class="col-md-9">
-                <div class="float-right mt-4">Logout</div>
-                <div class="float-right mt-4 mr-4">Login</div>
-                <div class="float-right mt-4 mr-4">Pelanggan </div>
-                <div class="float-right mt-4 mr-4">Daftar </div>
+            <div class="col md-9">
 
+                 <div class="float-right mt-4"><a href="?log=logout">Logout</a></div>
+                 <div class="float-right mt-4 mr-4">User : <a href="?user&m=updateuser&id"></a></div>
+                 <div class="float-right mt-4 mr-4">LEVEL : <?php echo $_SESSION['level']?></a></div>
+
+
+
+            </div>
+
+            <div class="col-mt-5">
+               <div class="col-md-3">
+
+                <ul class="nav flex-column">
+
+                <?php 
+                
+                  $level= $_SESSION['level'];
+                  switch ($level) {
+                    case 'admin':
+                        echo '
+                        
+                    <li class="nav-item"><a class="nav-link" href="?f=kategori&m=select">Kategori</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=menu&m=select">Menu</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=pelanggan&m=select">Pelanggan</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=order&m=select">Order</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=ordetail&m=select">OrderDetail</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?f=user&m=select">User</a></li>
+                        
+                        
+                        
+                        ';
+                        break;
+
+                        case 'kasir':
+                             echo'
+                             
+                             <li class="nav-item"><a class="nav-link" href="?f=order&m=select">Order</a></li>
+                             <li class="nav-item"><a class="nav-link" href="?f=ordetail&m=select">OrderDetail</a></li>
+                             
+                             
+                            ';
+                             
+                        break;
+
+                        case 'koki':
+                            echo'
+                            
+                            
+                            <li class="nav-item"><a class="nav-link" href="?f=ordetail&m=select">OrderDetail</a></li>
+                            
+                            
+                           ';
+                            
+                       break;
+                   
+                    
+                    default:
+                       echo'tidak ada menu';
+                        break;
+                  }
+                
+                ?>
+
+                    
+
+
+
+
+
+
+                </ul>
+
+
+
+
+
+
+
+
+               </div>
 
             </div>
 
--- restoran/orderdetail/select.php
+++ restoran/orderdetail/select.php
@@ -3,12 +3,12 @@
 
 <div class="form group">
   <form action="" method="post">
-  <div class="form group w-50">
+  <div class="form group w-50 float left">
     <label for="">Tanggal Awal</label>
     <input type="date" name="tawal" required  class="form-control" >
   </div>
 
-  <div class="form group w-50">
+  <div class="form group w-50 float left">
     <label for="">Tanggal Akhir</label>
     <input type="date" name="takhir" required  class="form-control" >
   </div>
@@ -27,7 +27,7 @@
 
   
   $jumlahdata = $db->rowCOUNT("SELECT idorderdetail FROM vorderdetail");
-  $banyak = 2;
+  $banyak = 3;
 
   $halaman = ceil ($jumlahdata / $banyak);
 
