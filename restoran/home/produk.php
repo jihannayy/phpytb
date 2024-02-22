ac0a4aa344c519328e978f937146297a11c83940
jihannayy <140767336+jihannayy@users.noreply.github.com>
2024-02-22 14:54:19 +0700

    FRONT END - Mengatur Kategori Produk Aplikasi Restoran

---
+++ restoran/home/produk.php
@@ -0,0 +1,27 @@
+$row = $db->getALL($sql);
+    $no=1+$mulai;
+?>
+            <?php if(!empty($row)){?>
+            <?php foreach($row as $r):?>
+
+                <div class="card" style="width: 15rem; float:left; margin:10px;">
+                    <img style="height:150px" src="upload/><?php echo $r['gambar']?>" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $r['menu']?></h5>
+                        <p class="card-text"><?php echo $r['harga']?></p>
+                        <a class="btn btn-primary" href="?f=kategori&m=insert" role="button">BELI</a>
+            </div>
+            </div>
+            
+            <?php endforeach?>
+            <?php } ?>
+<div style="clear:both;">
+    <?php
+    
+                for ($i=1; $i <= $halaman ; $i++) { 
+                    echo '<a href="?f=home&m=produk&p'.$i.'">'.$i.'</a>'
+                    echo '&nbsp &nbsp &nbsp ';
+                }
+    
+    ?>
+</div>
