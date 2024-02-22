de7fa0b302f060541a11aed4a4d4713a997e820c
jihannayy <140767336+jihannayy@users.noreply.github.com>
2024-02-22 21:51:00 +0700

    Menambah Dan Menghapus isi Cart Aplikasi Restoran

--- restoran/home/beli.php
+++ restoran/home/beli.php
@@ -3,9 +3,24 @@
 if (isset($_GET['hapus'])) {
     $id=$_GET['hapus'];
     unset($_SESSION['_'.$id]);
+  }
 
-    
+ if (isset($_GET['tambah'])) {
+    $id =$_GET['tambah'];
+    $_SESSION['_'.$id]++;
+
+   
  }
+
+ if (isset($_GET['kurang'])) {
+    $id =$_GET['kurang'];
+    $_SESSION['_'.$id]--;
+
+    if ($_SESSION['_'.$id]==0) {
+        unset($_SESSION['_'.$id]);
+    }
+ }
+
 
  if (!isset($_SESSION['pelanggan'])) {
     header("location:?f=home&m=login");
@@ -26,6 +41,7 @@ if (isset($_GET['hapus'])) {
 
 
    function isi($id){
+
     if (isset($_SESSION['_'.$id])) {
         $_SESSION['_'.$id]++;
     }else{
@@ -37,9 +53,11 @@ if (isset($_GET['hapus'])) {
 
     global $db;
 
+    $total=0;
+
     echo '
     
-    <table class="table table-bordered w-50">
+    <table class="table table-bordered w-700">
 
         <tr>
             <th>Menu</th>
@@ -67,16 +85,23 @@ if (isset($_GET['hapus'])) {
                 echo '<tr>';
                 echo '<td>'.$r['menu'].'</td>';
                 echo '<td>'.$r['harga'].'</td>';
-                echo '<td>'.$value.'</td>';
+                echo '<td><a href="?f-home&m=beli&tambah='.$r['idmenu'].'">[+]</a> &nbsp &nbsp'.$value.'&nbsp &nbsp <a href ="?f-home&m=beli&kurang='.$r['idmenu'].'">[-]</a></td>';
                 echo '<td>'.$r['harga'] * $value.'</td>';
                 echo '<td><a href="?f=home&m=beli&hapus='.$r['idmenu'].'">Hapus</a></td>';
                 echo '</tr>';
+                $total= $total + ($value * $r['harga']);
             }
 
            
         }
         
     }
+
+    echo '<tr>
+
+        <td colspan=4><h3>GRAND TOTAL : </h3></td>
+        <td><h3>'.$total.'</h3></td>
+    </tr>';
 
     echo '</table>';
    }
